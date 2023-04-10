6aa7bf90fe24794170074a85a34bead4f4e207b4
mehrdadrahimi99 <rms.rahimi@gmail.com>
2023-04-10 10:35:13 +0330

    Debug second loop

--- name_play.py
+++ name_play.py
@@ -7,6 +7,7 @@ big_name = re_name + name
 x = int(len(big_name)/2) - 1
 y = x + 1
 print(" "*int(x), big_name[x:y+1])
+
 x -= 1
 y += 1
 
@@ -17,14 +18,23 @@ for i in big_name:
     if x < 0 or y >= len(big_name):
         
         break
+
 x = 0
 y = len(big_name) - 1
 
 for i in big_name:
     print(" "*int(x), big_name[x], " "*int(y-(x+1)-2), big_name[y])
     x += 1
+    #print (x)
     y -= 1
-    if x == y == len(big_name)//2:
-        break
+    #print (y)
 
-print(" "*int(x), big_name[x]*2)
+    if x == (len(big_name)/2)-1:
+        break
+    #if x > y:
+    #    break
+
+print(" "*int(x), big_name[x:y+1])
+
+#print(" "*int(x), big_name[x]*2)
+#print (f'len big name is {len(big_name)}')
